9bce7c4b0a2dfdaed27f35ceccb11740a321b382
leayl <lea_yl@163.com>
2018-08-28 20:22:28 +0800

    'modify'

--- day1/tornado_params.py
+++ day1/tornado_params.py
@@ -8,26 +8,8 @@ from tornado.web import Application, RequestHandler
 
 class IndexHandler(RequestHandler):
     def get(self, *args, **kwargs):
-        msg = self.get_query_argument('msg', None)
-        if msg == 'fail':
-            html = '<form method="post" action=/login>' \
-             '<span>用户名：</span><input type="text" name=username><br>' \
-             '<span>密码：</span><input type="password" name = password><br>' \
-             '<input type="submit" value=提交>&nbsp;&nbsp;&nbsp;&nbsp;' \
-             '<input type="reset" value=重置>' \
-             '<p>用户名或密码错误</p>' \
-             '</form>'
-        else:
-            html = '<form method="post" action=/login enctype=multipart/form-data>' \
-             '<span>用户名：</span><input type="text" name=username><br>' \
-             '<span>密码：</span><input type="password" name = password><br>' \
-             '<input type=file name=avatar><br>' \
-             '<input type=file name=avatar><br>' \
-             '<input type="submit" value=提交>&nbsp;&nbsp;&nbsp;&nbsp;' \
-             '<input type="reset" value=重置>' \
-             '</form>'
+        self.render('login.html')
 
-        self.write(html)
 
     def post(self, *args, **kwargs):
         pass
@@ -122,7 +104,7 @@ app = Application([
     ('/', IndexHandler),
     ('/login', LoginHandler),
     ('/blog', BlogHandler), # 可以在后面以字典的方式传入初始化参数，在nitialize方法中接受处理
-])
+], template_path='templates')
 server = HTTPServer(app)
 define('port', type=int, default=8888)
 parse_config_file('../config/conf')
